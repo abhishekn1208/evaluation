0f9c58ad17d5d479b3294b6a51524048a105299f
abhishekn1208 <abhisheknigam735@gmail.com>
2024-06-03 03:14:16 +0530

    first problem

---
+++ question1.js
@@ -0,0 +1,8 @@
+const animals = ["Dog", "Cat", "Elephant", "Giraffe", "Lion", "Tiger", "Zebra", "Kangaroo", "Panda", "Monkey"];
+
+let extractSubArray =()=>{
+    let out1 = animals.slice(2,7)
+    return out1
+
+}
+console.log(extractSubArray(animals))
